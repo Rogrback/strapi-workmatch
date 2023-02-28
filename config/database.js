7c7a203a80056d317e45fc1693b94fb89d5df596
Rogrback <rfzt96@gmail.com>
2023-02-28 18:54:09 -0500

    fix bug

--- config/database.js
+++ config/database.js
@@ -9,13 +9,5 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL'),
     },
-    // connection: {
-    //   host: env('DATABASE_HOST', 'dpg-cfpl91h4rebfdatpbe9g-a.oregon-postgres.render.com'),
-    //   port: env.int('DATABASE_PORT', 5432),
-    //   database: env('DATABASE_NAME', 'sample_p3ii'),
-    //   user: env('DATABASE_USERNAME', 'admin'),
-    //   password: env('DATABASE_PASSWORD', 'BQHWL2KThukzwF5K74Sbtth16nya680I'),
-    //   ssl: env.bool('DATABASE_SSL', true),
-    // },
   },
 });
